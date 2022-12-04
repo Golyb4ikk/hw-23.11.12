431f920d098373492ae93515a2462e8dc5427ab2
Golyb4ik <only_game2@mail.ru>
2022-12-05 00:24:27 +0300

    1.3

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         //zadacha1
-        int sal = 15_000;
-        int all = 0;
-        int month = 0;
-        while (all < 12_000_000) {
-            month++;
-            int proc = all / 100 * 7;
-            all = all + sal + proc;
-            System.out.println("Месяц=" + month + " равен " + all);
-        }
-        //zadacha 2
-        int sal2 = 15_000;
-        int all2 = 0;
-        int month2 = 0;
-        while (all2 < 12_000_000) {
-            month2++;
-            int proc2 = all2 / 100 * 7;
-            all2 = all2 + sal2 + proc2;
-            if (month2 % 6 == 0) {
-                System.out.println("Месяц=" + month2 + " равен " + all2);
+        int yearStart=1850;
+        int yearFinish=2100;
+        int year=0;
 
-                //zadacha 3
-                int sal3 = 15_000;
-                int all3 = 0;
-
-                int halfYear = 0;
-                while (halfYear < 18) {
-                    halfYear++;
-
-                    int proc3 = all3 / 100 * 7;
-                    all3 = all3 + sal3 + proc3;
-
-                    System.out.println("Полгода=" + halfYear + " равен " + all3);
-                }
+        while(year<yearFinish){
+            year=year+79;
+            if(yearStart<year&&year<yearFinish){
+                System.out.println("Нужный год "+year);
             }
-            //zadacha 4
-            int first = 1;
-            for (first = 1; first < 31; first = first + 7) {
-                System.out.println("Сегодня пятница, "+first+"-е число. Необходимо подготовить отчет.");
-            }
-        }
-    }
 
+        }
+        //zadacha2
+        int a=2;
+        int y=0;
+        for(int i=0;i<10;i++){
+            y++;
+            int c=a*y;
+            System.out.println(a+""+"*"+y+"="+c);
+        }
+
+     }
     }
