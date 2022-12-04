83852379f4d231e357c6c1403bad72c2822e69c9
Golyb4ik <only_game2@mail.ru>
2022-12-05 00:56:31 +0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,37 @@
 public class Main {
     public static void main(String[] args) {
         //zadacha1
-        int yearStart=1850;
-        int yearFinish=2100;
-        int year=0;
-
-        while(year<yearFinish){
-            year=year+79;
-            if(yearStart<year&&year<yearFinish){
-                System.out.println("Нужный год "+year);
-            }
+        int sal=15000;
+        int all=0;
+        int month=0;
+        while(all<2459000){
+            month++;
+            all=all+sal;
+            System.out.println("Месяц "+month+" , сумма накоплений равна "+all+" рублей");
 
         }
         //zadacha2
-        int a=2;
-        int y=0;
-        int c;
-        for(int i=0;i<10;i++){
-            y++;
-            c=a*y;
-            System.out.println(a+""+"*"+y+"="+c);
+        int i=0;
+        while(i<10){
+            i++;
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for(int p=10;p>0;p--){
+            System.out.print(p+" ");
+        }
+        //zadacha3
+        System.out.println();
+        int sal2 = 12_000_000;
+
+        int year = 0;
+        int die=all / 1000 * 8;
+        int life=all / 1000 * 17;
+        while (year < 10) {
+            year++;
+            sal2 = sal2 - die + life;
+            System.out.println("Год " + year + " равен " + sal2);
         }
 
-     }
     }
+}
